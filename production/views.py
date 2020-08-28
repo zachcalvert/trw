577fade72d73aa3f9d42cf0c488d8074a4cacade
Zach Calvert <zcalvert@oreilly.com>
2020-08-28 09:21:02 -0700

    Highlight missed checkpoints on dashboard

--- production/views.py
+++ production/views.py
@@ -1,9 +1,19 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 from production.models import WorkOrder
 
 
 def target_dashboard(request):
+    today = datetime.today()
     active_orders = WorkOrder.objects.filter(active=True)
-    context = {'orders': active_orders}
+
+    missed_checkpoints = []
+    for order in active_orders:
+        missed_checkpoints.extend(
+            list(order.checkpoints.filter(date__lt=today, goal__gt=order.current).values_list('id', flat=True))
+        )
+
+    context = {'today': datetime.today(), 'orders': active_orders, 'missed_checkpoints': missed_checkpoints}
     return render(request, 'production/dashboard.html', context)
--- trw/settings.py
+++ trw/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'adminsortable2',
     'django_extensions',
 
     'brands',
