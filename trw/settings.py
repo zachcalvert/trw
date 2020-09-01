d319151b0bbd1f68ce2936b2bea09b4b5bef2b37
Zach Calvert <zcalvert@oreilly.com>
2020-09-01 12:51:03 -0700

    Add staticfiles dir

--- trw/settings.py
+++ trw/settings.py
@@ -127,5 +127,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
