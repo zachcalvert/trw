bbb2f7c0f458db00bf50deb9dbfb7483bf942005
Zach Calvert <zcalvert@oreilly.com>
2020-10-13 14:20:46 -0700

    csrf exempt the new message view

--- bellybot/views.py
+++ bellybot/views.py
@@ -1,23 +1,24 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from bellybot.models import GroupMeBot
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def new_message(request):
     # if 'group_id' not in request.POST or request.POST['group_id'] != '63320852':
     #     return HttpResponse(status=204)
 
     try:
-        bot = GroupMeBot()
         message_content = request.POST['text']
         sender = request.POST['name']
     except KeyError:
         print('ERROR parsing GroupMe message: {}'.format(request.POST))
         return
 
-    bot.respond(sender, message_content)
+    GroupMeBot().respond(sender, message_content)
     return HttpResponse(status=204)
 
 
