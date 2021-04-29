e8905ca043e30a6f3ad191e96de5500ae70afea8
Zach Calvert <zcalvert@oreilly.com>
2021-04-29 11:05:24 -0400

    update admin texts

--- server/trw/urls.py
+++ server/trw/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('production.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = "TRW Admin"
+admin.site.site_title = "TRW Admin Portal"
+admin.site.index_title = "Welcome to the TRW Work Order Admin"
